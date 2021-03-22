b9d84f630a89d41a77eb90bcc12769f4ff00cc70
dayant-wood <dayant.wood@gmail.com>
2021-03-22 16:49:45 +0300

    create: pages.html

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+    //переключение линков
+    let linkItem = $(".fly-sheet__item");
+    linkItem.on("click", function(event) {
+
+        linkItem.removeClass("fly-sheet__item--active");
+        $(this).addClass("fly-sheet__item--active");
+    });
+
+});
