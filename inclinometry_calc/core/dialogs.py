abdbc8dd7dd9de9a5582424a5e58bcc1e8c3d927
Michael <mihail.tchernov@yandex.ru>
2021-11-12 23:29:08 +0300

    Проект пересобран для удобной упаковки

---
+++ inclinometry_calc/core/dialogs.py
@@ -0,0 +1,24 @@
+from PyQt5.QtWidgets import *
+
+
+def show_message(message_text):
+    msg = QMessageBox()
+    msg.setIcon(QMessageBox.Information)
+    msg.setText(message_text)
+    msg.exec_()
+
+
+def show_file_dialog():
+    file_dialog = QFileDialog()
+    file_dialog.setFileMode(QFileDialog.ExistingFile)
+    file_list = file_dialog.getOpenFileNames()[0]
+    if len(file_list) == 1:
+        return file_list[0]
+    else:
+        return None
+
+
+def show_folder_dialog():
+    file_dialog = QFileDialog()
+    folder_path = file_dialog.getExistingDirectory()
+    return folder_path
